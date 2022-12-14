c7dc013f7d078c1887712817719b5436228a8729
NotBDR <ibdr99@gmail.com>
2022-12-14 21:09:59 +0300

    middlewares loggin and security

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const PORT = 3000;
+//create instance server
 const app = (0, express_1.default)();
+//HTTP request logger middleware
+app.use((0, morgan_1.default)('common'));
 //add routing
 app.get('/', (req, res) => {
     res.json({ Message: 'h' });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, request, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const PORT = 3000;
+
+//create instance server
 const app: Application = express();
+//HTTP middleware
+app.use(express.json());
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        message: 'to many requests!',
+    })
+);
 
 //add routing
-
 app.get('/', (req: Request, res: Response) => {
     res.json({ Message: 'h' });
+});
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.json({ Message: 'hello from post', data: request.body });
 });
 
 app.listen(PORT, () => {
